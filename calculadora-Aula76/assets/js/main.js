0f0ef8e202024413fd848dea2d4bc2086525f4d0
Gilberto Junior <gagjunior@hotmail.com>
2022-03-08 13:32:12 -0300

    Criando o style da calculadora

---
+++ calculadora-Aula76/assets/js/main.js
@@ -0,0 +1,18 @@
+function criaCalculadora(){
+    return {
+        display: document.querySelector('.display'),
+
+        iniciar(){
+            alert('Iniciando...');
+        },
+
+        cliqueBotoes(){
+            document.addEventListener('click', e =>{
+                
+            } )
+        }
+    }
+}
+
+const calculadora = criaCalculadora();
+calculadora.iniciar();
