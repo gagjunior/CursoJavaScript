f44fcf848281912b7c472e833976190012476ccc
gagjunior <gagjunior@hotmail.com>
2022-03-20 22:22:58 -0300

    Criar botão para gera novo CPF

--- gerador_cpf/src/main.js
+++ gerador_cpf/src/main.js
@@ -1,1 +1,16 @@
+import GeraCPF from './modules/GeraCPF'
 import './assets/css/style.css'
+
+(function () {
+
+    document.addEventListener('click', e => {
+        const elemento = e.target
+        if (elemento.classList.contains('btn-gerar')) {
+            const gera = new GeraCPF()
+            const cpfGerado = document.querySelector('.cpf-gerado');
+            cpfGerado.innerHTML = gera.geraNovoCpf();
+        }
+    })
+
+
+})();
